760576b948c7dd29c1670ad4dc2c41c0ca4ddd88
Caitlin Dyke <caitlin@mobilefringe.com>
2019-03-07 14:00:04 -0500

    bable update

--- nuxt.config.js
+++ nuxt.config.js
@@ -107,6 +107,9 @@ plugins: [
 
   /* Build configuration */
   build: {
+    babel: {
+      presets: ['es2015', 'stage-0']
+    },
     /* You can extend webpack config here */
     extend(config, ctx) {
       
